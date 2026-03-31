b9f411e1e50f5b6467a60840c9b3b7a44fa81cb7
Ceyda Yilmaz <ceyda.yilmaz@student.kdg.be>
2026-03-31 11:42:48 +0200

    update contact.php

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -2,8 +2,10 @@
 
 namespace App\Models;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Contact extends Model
 {
@@ -17,4 +19,9 @@ class Contact extends Model
         'phone',
         'address',
     ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
