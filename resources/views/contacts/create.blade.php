bcbac73e9ae5a124dbe01b851096405f1d4ef027
Ceyda Yilmaz <ceyda.yilmaz@student.kdg.be>
2026-04-02 11:12:21 +0200

    edit pagina gegevens

---
+++ resources/views/contacts/create.blade.php
@@ -0,0 +1,38 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <h1>Nieuw contact</h1>
+
+    <form action="{{ route('contacts.store') }}" method="POST">
+        @csrf
+
+        <div class="mb-3">
+            <label for="first_name" class="form-label">Voornaam</label>
+            <input type="text" name="first_name" id="first_name" class="form-control">
+        </div>
+
+        <div class="mb-3">
+            <label for="last_name" class="form-label">Achternaam</label>
+            <input type="text" name="last_name" id="last_name" class="form-control">
+        </div>
+
+        <div class="mb-3">
+            <label for="email" class="form-label">Email</label>
+            <input type="email" name="email" id="email" class="form-control">
+        </div>
+
+        <div class="mb-3">
+            <label for="phone" class="form-label">Telefoon</label>
+            <input type="text" name="phone" id="phone" class="form-control">
+        </div>
+
+        <div class="mb-3">
+            <label for="address" class="form-label">Adres</label>
+            <input type="text" name="address" id="address" class="form-control">
+        </div>
+
+        <button type="submit" class="btn btn-primary">Opslaan</button>
+    </form>
+</div>
+@endsection
