8a516ff619ae872f8f6dbedb84e10150b2787d32
Ceyda Yilmaz <ceyda.yilmaz@student.kdg.be>
2026-04-02 11:35:38 +0200

    update routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContactController;
+use App\Http\Controllers\HomeController;
 
 Route::get('/', function () {
     return redirect('/login');
@@ -9,4 +11,24 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+Route::get('/contacts/create', [ContactController::class, 'create'])
+    ->middleware('auth')
+    ->name('contacts.create');
+
+Route::post('/contacts', [ContactController::class, 'store'])
+    ->middleware('auth')
+    ->name('contacts.store');
+
+Route::get('/contacts/{contact}/edit', [ContactController::class, 'edit'])
+    ->middleware('auth')
+    ->name('contacts.edit');
+
+Route::put('/contacts/{contact}', [ContactController::class, 'update'])
+    ->middleware('auth')
+    ->name('contacts.update');
+
+Route::delete('/contacts/{contact}', [ContactController::class, 'destroy'])
+    ->middleware('auth')
+    ->name('contacts.destroy');
